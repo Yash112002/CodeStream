ea73f4bcba788bdbc91e6c33dea5fcc718702d56
Yash <77615518+Yash112002@users.noreply.github.com>
2023-07-04 07:33:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server started at Port : ${PORT}`);
 });
